5d9a13716e0b406b0eaffd8c4c538369090289cc
tcwhitak <t.chad.whitaker@gmail.com>
2018-09-05 22:58:27 -0400

    inputs eval and display result in history above current display

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-class App extends Component {
+class App extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      input: ''
+      input: '',
+      history: []
     }
   }
   render () {
+    console.log(this.state.history)
     return (
       <div className='App'>
         <Calculator
           onClick={(i) => this.handleClick(i)}
           input={this.state.input}
+          history={this.state.history}
         />
       </div>
 
@@ -24,10 +27,84 @@ class App extends Component {
 
   handleClick (i) {
     const input = this.state.input
-    this.setState({
-      input: input.concat([i.toString()]
-      )
-    })
+    if (input === '' && i === '.') {
+      this.setState({
+        input: input.concat('0' + i)
+      })
+    } else if (input === '' && i === '%') {
+      this.setState({
+        input: input.concat('0' + i)
+      })
+    } else if (i !== '=' && i !== 'AC') {
+      this.setState({
+        input: input.concat([i.toString()])
+      })
+    } else if (i === '=') {
+      let result = eval(input)
+      this.setState({
+        history: this.state.history.concat(`${input.toString()} = ${result.toString()}`),
+        input: result.toString()
+      })
+    } else if (i === 'AC') {
+      this.setState({
+        input: ''
+      })
+    }
+  }
+}
+
+class Calculator extends React.Component {
+  renderButton (i) {
+    return <CalButton
+      value={[i]}
+      onClick={() => this.props.onClick(i)}
+    />
+  }
+
+  renderDisplay () {
+    return <Display
+      input={this.props.input}
+    />
+  }
+
+  render () {
+    return (
+      <div className='calculator'>
+        <div id='history'>{this.props.history.map((h, idx) => <div key={idx}>{h}</div>)}</div>
+        <div className='calc-wrapper'>
+          {this.renderDisplay()}
+          <div className='calc-row'>
+            {this.renderButton('AC')}
+            {this.renderButton('+/-')}
+            {this.renderButton('%')}
+            {this.renderButton('/')}
+          </div>
+          <div className='calc-row'>
+            {this.renderButton(7)}
+            {this.renderButton(8)}
+            {this.renderButton(9)}
+            {this.renderButton('*')}
+          </div>
+          <div className='calc-row'>
+            {this.renderButton(4)}
+            {this.renderButton(5)}
+            {this.renderButton(6)}
+            {this.renderButton('-')}
+          </div>
+          <div className='calc-row'>
+            {this.renderButton(1)}
+            {this.renderButton(2)}
+            {this.renderButton(3)}
+            {this.renderButton('+')}
+          </div>
+          <div className='calc-row'>
+            {this.renderButton(0)}
+            {this.renderButton('.')}
+            {this.renderButton('=')}
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
@@ -47,58 +124,6 @@ class CalButton extends React.Component {
       <button className='calButton' onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
-    )
-  }
-}
-
-class Calculator extends Component {
-  renderButton (i) {
-    return <CalButton
-      value={[i]}
-      onClick={() => this.props.onClick(i)}
-    />
-  }
-
-  renderDisplay () {
-    return <Display
-      input={this.props.input}
-    />
-  }
-
-  render () {
-    return (
-      <div className='calculator'>
-        {this.renderDisplay()}
-        <div className='calc-row'>
-          {this.renderButton('AC')}
-          {this.renderButton('+/-')}
-          {this.renderButton('%')}
-          {this.renderButton('/')}
-        </div>
-        <div className='calc-row'>
-          {this.renderButton(7)}
-          {this.renderButton(8)}
-          {this.renderButton(9)}
-          {this.renderButton('*')}
-        </div>
-        <div className='calc-row'>
-          {this.renderButton(4)}
-          {this.renderButton(5)}
-          {this.renderButton(6)}
-          {this.renderButton('-')}
-        </div>
-        <div className='calc-row'>
-          {this.renderButton(1)}
-          {this.renderButton(2)}
-          {this.renderButton(3)}
-          {this.renderButton('+')}
-        </div>
-        <div className='calc-row'>
-          {this.renderButton(0)}
-          {this.renderButton('.')}
-          {this.renderButton('=')}
-        </div>
-      </div>
     )
   }
 }
